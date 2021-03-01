396edbeee0f8ca7bf28667504abad9ce04aa1262
krisnemeth <krsnmth@gmail.com>
2021-03-01 13:50:43 +0000

    created custom hooks

--- src/Home.js
+++ src/Home.js
@@ -1,22 +1,18 @@
-import { useState } from 'react';
+
+import { useState, useEffect } from 'react';
+import BlogList from './BlogList';
+import useFetch from './useFetch';
 
 const Home = () => {
-    
-    const [name, setName] = useState('mario');
-    const [age, setAge] = useState(13);
+    const { data: blogs, isPending, error } = useFetch('http://localhost:8000/blogs');
 
-    const handleClick = () => {
-        setName('luigi');
-        setAge('18');
-    }
-    
     return ( 
         <div className="home">
-            <h2>Homepage</h2>
-            <p>{ name } is { age } old.</p>
-            <button onClick={handleClick}>Click me</button>
+            { error && <div>{ error }</div> }
+            { isPending && <div>Loading...</div> }
+            { blogs && <BlogList blogs={blogs} title="All Blogs"/> }
         </div>
     );
 }
- 
+
 export default Home;
